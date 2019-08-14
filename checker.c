cea97f103f0d31abe7ee7d2904564d5cdde552f6
Danie LINDE <dlinde@c4r18s2.wethinkcode.co.za>
2019-08-14 11:45:22 +0200

    progress

--- checker.c
+++ checker.c
@@ -1,29 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlinde <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/28 14:46:50 by dlinde            #+#    #+#             */
-/*   Updated: 2019/07/04 12:43:37 by dlinde           ###   ########.fr       */
+/*   Created: 2019/08/14 10:48:06 by dlinde            #+#    #+#             */
+/*   Updated: 2019/08/14 11:44:41 by dlinde           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft/libft.h"
+#include <fcntl.h>
+#include <stdlib.h>
 
-void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+int	main(int ac, char **av)
 {
-	t_list	*l1;
-	t_list	*l2;
+	int 	list[ac - 1];
+	int 	fd;
+	int		n;
+	char	*line;
 
-	l1 = *alst;
-	while (l1 && del)
+	n = 0;
+	if (ac != 1)
 	{
-		l2 = l1->next;
-		del(l1->content, l1->content_size);
-		free(l1);
-		l1 = l2;
+		while (n <= ac - 2)
+		{
+			if ((list[n] = ft_atoi(av[n + 1])))
+				n++;
+			else
+			{
+				ft_putendl("ERROR");
+				exit(0);
+			}
+		}
+		fd = open("com", O_RDONLY);
+		while (get_next_line(fd, &line) == 1)
+		{
+			ft_putendl(line);
+			free(line);
+		}
 	}
-	*alst = NULL;
 }
